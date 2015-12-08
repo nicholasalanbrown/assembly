2b975cacc304c94311c7db36d88d462ecb1c56d2
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2015-12-07 23:08:05 -0500

    More view wiring

---
+++ application/components/myProfile.js
@@ -0,0 +1,42 @@
+import React from 'react-native';
+import globals from '../styles/globalStyles';
+
+let {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} = React;
+
+class myProfile extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  _nextRoute() {
+    this.props.navigator.push({
+      title: 'Home',
+      component: Home
+    })
+  }
+  render(){
+      return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={this._nextRoute} style={globals.button}>
+              <Text style={globals.buttonText}>Event</Text>
+            </TouchableOpacity>
+        </View>
+      )
+    }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+});
+
+module.exports = myProfile;
--- application/components/welcome.js
+++ application/components/welcome.js
@@ -3,9 +3,15 @@ import globals from '../styles/globalStyles';
 import Home from './home';
 import Create from './create';
 import myGroups from './myGroups';
+import userProfile from './userProfile';
+import chat from './chat';
+import myProfile from './myProfile';
+import viewGroup from './viewGroup';
+import viewEvent from './viewEvent';
 
 let {
   View,
+  ScrollView,
   Text,
   TouchableOpacity,
   StyleSheet,
@@ -20,7 +26,7 @@ class Welcome extends React.Component{
   render(){
     console.log(this.props);
       return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <TouchableOpacity onPress={() =>{
                 this.props.navigator.push({
                   title: 'Home',
@@ -51,7 +57,47 @@ class Welcome extends React.Component{
             style={globals.button}>
               <Text style={globals.buttonText}>My Groups</Text>
             </TouchableOpacity>
-        </View>
+            <TouchableOpacity onPress={() =>{
+                this.props.navigator.push({
+                  title: 'My Groups',
+                  component: myGroups
+                })
+              }
+            }
+            style={globals.button}>
+              <Text style={globals.buttonText}>User Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() =>{
+                this.props.navigator.push({
+                  title: 'Chat',
+                  component: chat
+                })
+              }
+            }
+            style={globals.button}>
+              <Text style={globals.buttonText}>Chat</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() =>{
+                this.props.navigator.push({
+                  title: 'View Group',
+                  component: viewGroup
+                })
+              }
+            }
+            style={globals.button}>
+              <Text style={globals.buttonText}>View Group</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() =>{
+                this.props.navigator.push({
+                  title: 'View Event',
+                  component: viewEvent
+                })
+              }
+            }
+            style={globals.button}>
+              <Text style={globals.buttonText}>View Event</Text>
+            </TouchableOpacity>
+        </ScrollView>
       )
     }
 };
@@ -59,8 +105,6 @@ class Welcome extends React.Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
 });
 
