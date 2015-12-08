6c1caafcf57b878e468f6744b256e032ddcc5024
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2015-12-07 23:11:22 -0500

    REmoving console.log

--- application/components/welcome.js
+++ application/components/welcome.js
@@ -24,7 +24,6 @@ class Welcome extends React.Component{
     }
   }
   render(){
-    console.log(this.props);
       return (
         <ScrollView style={styles.container}>
             <TouchableOpacity onPress={() =>{
