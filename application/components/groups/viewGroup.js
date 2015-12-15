616f8a66e0ed5479e267101e2b74a451993a49dd
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2015-12-15 09:30:24 -0500

    Progress towards group users

--- application/components/groups/viewGroup.js
+++ application/components/groups/viewGroup.js
@@ -1,6 +1,7 @@
 import React from 'react-native';
 import Globals from '../../styles/globals';
 import Hero from '../shared/hero';
+import _ from 'underscore';
 
 let {
   View,
@@ -13,10 +14,60 @@ let {
 class ViewGroup extends React.Component{
   constructor(props){
     super(props);
+    this._addUserstoGroup = this._addUserstoGroup.bind(this);
     this.state = {
     }
   }
+  _addUserstoGroup() {
+    this.props.loading(true);
+    fetch("http://localhost:2403/users/{$limit: 10}", {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.errors) {
+                this.props.loading(false);
+                console.log(data.errors);
+            }
+            else {
+                let groupUsers = data;
+            }
+        })
+        .catch((error) => console.log(error))
+        .done();
+    _.each(groupUsers, function(groupUser) {
+      fetch("http://localhost:2403/groups/"+this.props.groupData.id, {
+              method: "PUT",
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({groupMembers: {$push: groupUser.id}})
+          })
+          .then((response) => response.json())
+          .then((data) => {
+              if (data.errors) {
+                  this.props.loading(false);
+                  console.log(data.errors);
+              }
+              else {
+                  this.props.loading(false);
+                  console.log(data);
+              }
+          })
+          .catch((error) => console.log(error))
+          .done();
+    })
+  }
+  componentWillMount() {
+    this._addUserstoGroup();
+  }
   render(){
+    console.log(this.props);
       return (
         <ScrollView style={styles.container}>
           <Hero title={this.props.groupData.groupName} />
