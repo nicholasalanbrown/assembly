fa0e41553389849a8073f4d2ff01ea10b7e58451
tomatagolo <tom@agolo.com>
2015-12-28 21:59:25 -0500

    get login to work properly with navigator

--- brn-build-1/index.ios.js
+++ brn-build-1/index.ios.js
@@ -1,7 +1,6 @@
 import React from 'react-native';
 import _ from 'underscore';
 import FBLogin from 'react-native-facebook-login';
-
 import Welcome from './application/components/welcome';
 import Home from './application/components/home';
 import Colors from './application/styles/colors';
@@ -24,10 +23,11 @@ let {
   Dimensions,
   AsyncStorage,
   StatusBarIOS,
+  Navigator,
   DeviceEventEmitter
 } = React;
 
-let {FBLoginManager} = NativeModules;
+let {FBLoginManager,} = NativeModules;
 
 class Assembly extends React.Component{
   constructor(props){
@@ -36,16 +36,47 @@ class Assembly extends React.Component{
       loading: false,
       uiBlocker: false,
       user: {},
-      modal: false,
-      modalProps: {title: "Technology Tags"},
+      initialRoute: 'home',
     }
   }
-  _toggleLoading (bool) {
+  componentWillMount () {
+    DeviceEventEmitter.addListener(
+      FBLoginManager.Events["Logout"],
+        (eventData) => {
+        this.setState({user: {}});
+    });
+    DeviceEventEmitter.addListener(FBLoginManager.Events["Login"], (eventData) => {
+      let user = this.state.user;
+      user.userId = eventData.credentials.userId;
+      user.token = eventData.credentials.token;
+      user.tokenExpirationDate = eventData.credentials.tokenExpirationDate;
+      this.setState({user: user});
+      this._findUser(user.userId);
+    });
+    DeviceEventEmitter.addListener(FBLoginManager.Events["LoginFound"], (eventData) => {
+      let user = this.state.user;
+      user.userId = eventData.credentials.userId;
+      user.token = eventData.credentials.token;
+      user.tokenExpirationDate = eventData.credentials.tokenExpirationDate;
+      this.setState({user: user});
+      this._findUser(user.userId);
+    });
+    DeviceEventEmitter.addListener(FBLoginManager.Events["LoginNotFound"], (eventData) => {
+      console.log("login not found");
+      this.setState({user: {}})
+    });
+  }
+  _toggleLoading(bool) {
     this.setState({loading: bool});
   }
-  _toggleBlocker (content) {
+  _toggleBlocker(content) {
     this.setState({uiBlocker: content});
   }
+
+  _setUser(data) {
+    this.setState({user: data});
+  }
+
   _findUser (userId) {
     this._toggleLoading(true);
     if (!userId) {
@@ -53,7 +84,8 @@ class Assembly extends React.Component{
       let userId = this.state.user.userId;
     }
     console.log('finding user');
-    fetch(`${http://localhost:2403/users?userId=}${userId}`, {
+    let url = `http://localhost:2403/users?userId=${userId}`
+    fetch(url, {
       method: "GET"
     })
     .then((response) => response.json())
@@ -79,144 +111,147 @@ class Assembly extends React.Component{
     .catch((error) => console.log(error))
     .done();
   }
-  _createUser () {
-    this._toggleLoading(true);
-    let user = this.state.user;
-    user.username = "facebook_"+user.userId;
-    user.password = "password";
-    fetch("http://localhost:2403/users", {
-      method: "POST",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user)
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.errors) {
-        this._toggleLoading(false);
-        console.log(data.errors);
-      }
-      else {
-        this._toggleLoading(false);
-        this._getUserProfile(data.id);
-      }
-    })
-    .catch((error) => console.log(error))
-    .done();
-  }
-  _getUserProfile(id) {
-    this._toggleLoading(true);
-    let user = this.state.user;
-    let api = `https://graph.facebook.com/me?fields=id,name,email&access_token=${user.token}`;
-    fetch(api, {
-      method: "GET"
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.errors) {
-        this._toggleLoading(false);
-        console.log(data.errors);
-      }
-      else {
-        data.picture = "https://graph.facebook.com/"+user.userId+"/picture?type=large";
-        user.profile = data;
-        this.setState({user: user})
-        this._setUser(user);
-        this._updateUser(id);
-      }
-    })
-    .catch((error) => console.log(error))
-    .done();
-  }
-  _updateUser(id) {
-    this._toggleLoading(true);
-    fetch("http://localhost:2403/users/"+id, {
-      method: "PUT",
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(this.state.user)
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.errors) {
-        this._toggleLoading(false);
-        console.log(data.errors);
-      }
-      else {
-        this._toggleLoading(false);
-      }
-    })
-    .catch((error) => console.log(error))
-    .done();
-  }
-  _setUser (data) {
-    this.setState({user: data});
-  }
-  _openModal (props) {
-    this.setState({modalProps: props, modal: true});
-  }
-  componentWillMount () {
-    DeviceEventEmitter.addListener(
-      FBLoginManager.Events["Logout"],
-        (eventData) => {
-        this.setState({user: {}});
-    });
-    DeviceEventEmitter.addListener(FBLoginManager.Events["Login"], (eventData) => {
-      let user = this.state.user;
-      user.userId = eventData.credentials.userId;
-      user.token = eventData.credentials.token;
-      user.tokenExpirationDate = eventData.credentials.tokenExpirationDate;
-      this.setState({user: user});
-      this._findUser(user.userId);
-    });
-    DeviceEventEmitter.addListener(FBLoginManager.Events["LoginFound"], (eventData) => {
-      let user = this.state.user;
-      user.userId = eventData.credentials.userId;
-      user.token = eventData.credentials.token;
-      user.tokenExpirationDate = eventData.credentials.tokenExpirationDate;
-      this.setState({user: user});
-      this._findUser(user.userId);
-    });
-    DeviceEventEmitter.addListener(FBLoginManager.Events["LoginNotFound"], (eventData) => {
-      console.log("loginnotfound");
-      this.setState({user: {}})
-    });
-    // seedUsers(); // seed database
-  }
-  render() {
-    let {modal,} = this.state;
-    StatusBarIOS.setStyle('light-content');
-    let modal = modal ? <Modal title={this.state.modalProps.title} /> : null;
 
+  render(){
     return (
-      <View style={styles.container}>
-      <NavigatorIOS
+      <View style={{flex: 1,}}>
+      <Navigator
         style={styles.container}
-        barTintColor={Colors.brandPrimary}
-        titleTextColor='#ffffff'
-        tintColor='#ffffff'
-        initialRoute={{
-          component: Home,
-          title: 'Home',
-          passProps: {
-            loading: this._toggleLoading.bind(this),
-            uiBlocker: this._toggleBlocker.bind(this),
-            setUser: this._setUser.bind(this),
-            user: this.state.user
-          },
-        }}
-      />
-        {this.state.loading ? <Loading /> : null}
+        initialRoute={{name: this.state.initialRoute, index: 0}}
+        renderScene={(route, navigator) => {
+          switch(route) {
+            case 'welcome':
+              return <Welcome navigator={navigator}/>
+            break;
+            default:
+              return <Home
+                      uiBlocker={this._toggleBlocker.bind(this)}
+                      setUser={this._setUser.bind(this)}
+                      user={this.state.user}
+                      navigator={navigator}/>
+            break;
+          }
+        }}/>
+        {this.state.loading   ? <Loading />                               : null}
         {this.state.uiBlocker ? <UIBlocker text={this.state.uiBlocker} /> : null}
-        {Object.keys(this.state.user).length == 0 ? <Welcome loading={this._toggleLoading.bind(this)} setUser= {this._setUser.bind(this)}/> : null}
-        {modal}
+        {Object.keys(this.state.user).length == 0 ? <Welcome loading={this._toggleLoading.bind(this)} setUser={this._setUser.bind(this)}/> : null}
       </View>
-    );
+    )
+
   }
+  //
+  //
+  // _createUser () {
+  //   this._toggleLoading(true);
+  //   let user = this.state.user;
+  //   user.username = "facebook_"+user.userId;
+  //   user.password = "password";
+  //   fetch("http://localhost:2403/users", {
+  //     method: "POST",
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(user)
+  //   })
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     if (data.errors) {
+  //       this._toggleLoading(false);
+  //       console.log(data.errors);
+  //     }
+  //     else {
+  //       this._toggleLoading(false);
+  //       this._getUserProfile(data.id);
+  //     }
+  //   })
+  //   .catch((error) => console.log(error))
+  //   .done();
+  // }
+  // _getUserProfile(id) {
+  //   this._toggleLoading(true);
+  //   let user = this.state.user;
+  //   let api = `https://graph.facebook.com/me?fields=id,name,email&access_token=${user.token}`;
+  //   fetch(api, {
+  //     method: "GET"
+  //   })
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     if (data.errors) {
+  //       this._toggleLoading(false);
+  //       console.log(data.errors);
+  //     }
+  //     else {
+  //       data.picture = "https://graph.facebook.com/"+user.userId+"/picture?type=large";
+  //       user.profile = data;
+  //       this.setState({user: user})
+  //       this._setUser(user);
+  //       this._updateUser(id);
+  //     }
+  //   })
+  //   .catch((error) => console.log(error))
+  //   .done();
+  // }
+  // _updateUser(id) {
+  //   this._toggleLoading(true);
+  //   fetch("http://localhost:2403/users/"+id, {
+  //     method: "PUT",
+  //     headers: {
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(this.state.user)
+  //   })
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     if (data.errors) {
+  //       this._toggleLoading(false);
+  //       console.log(data.errors);
+  //     }
+  //     else {
+  //       this._toggleLoading(false);
+  //     }
+  //   })
+  //   .catch((error) => console.log(error))
+  //   .done();
+  // }
+  //
+  // _openModal (props) {
+  //   this.setState({modalProps: props, modal: true});
+  // }
+  //
+  //   // seedUsers(); // seed database
+  // }
+  // render() {
+  //   let {modal,} = this.state;
+  //   StatusBarIOS.setStyle('light-content');
+  //   let modalContent = modal ? <Modal title={this.state.modalProps.title} /> : null;
+  //
+  //   return (
+  //     <View style={styles.container}>
+  //     <NavigatorIOS
+  //       style={styles.container}
+  //       barTintColor={Colors.brandPrimary}
+  //       titleTextColor='#ffffff'
+  //       tintColor='#ffffff'
+  //       initialRoute={{
+  //         component: Home,
+  //         title: 'Home',
+  //         passProps: {
+  //           loading: this._toggleLoading.bind(this),
+  //           uiBlocker: this._toggleBlocker.bind(this),
+  //           setUser: this._setUser.bind(this),
+  //           user: this.state.user
+  //         },
+  //       }}
+  //     />
+  //       {this.state.loading ? <Loading /> : null}
+  //       {this.state.uiBlocker ? <UIBlocker text={this.state.uiBlocker} /> : null}
+  //       {Object.keys(this.state.user).length == 0 ? <Welcome loading={this._toggleLoading.bind(this)} setUser= {this._setUser.bind(this)}/> : null}
+  //       {modalContent}
+  //     </View>
+  //   );
+  // }
 };
 
 var styles = StyleSheet.create({
